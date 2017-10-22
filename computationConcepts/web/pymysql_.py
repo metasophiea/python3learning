406949df5245fc84269a181a76403c1d67f2cd22
metasophiea <metasophiea@gmail.com>
2017-10-22 22:28:59 +0100

    little work on database connections (now that I go that working, and I got a little excited)

--- computationConcepts/web/pymysql_.py
+++ computationConcepts/web/pymysql_.py
@@ -1,14 +1,103 @@
 import pymysql
+#   This module is used to connect to and work with a MySQL server. One can
+# execute all the regular SQL commands with it.
 
-connection = pymysql.connect (
-    host = "192.168.1.19",
-    user = "pythonProgram",
-    passwd = "woodensword",
-    db = "example"
+# # connecting to the server
+# connection = pymysql.connect (
+#     host = "192.168.1.19",
+#     user = "pythonProgram",
+#     passwd = "woodensword",
+#     db = "example"
+#     )
+# cursor = connection.cursor()
+
+# # closing conenctions
+# cursor.close()
+# connection.close()
+
+
+
+
+# # Get the server version data
+# connection = pymysql.connect(host = "192.168.1.19", user = "pythonProgram", passwd = "woodensword", db = "example")
+# cursor = connection.cursor()
+
+# cursor.execute ("SELECT VERSION()")
+# row = cursor.fetchone()
+# print("server version:", row[0])
+
+# cursor.close()
+# connection.close()
+
+
+
+
+# # Fetch all the data from the 'numbers' table and print it all out
+# connection = pymysql.connect(host = "192.168.1.19", user = "pythonProgram", passwd = "woodensword", db = "example")
+# cursor = connection.cursor()
+
+# cursor.execute("SELECT * FROM numbers")
+# for item in cursor.fetchall():
+#     print(item)
+
+# cursor.close()
+# connection.close()
+
+
+
+
+# Create a table and populate it
+# connect
+connection = pymysql.connect(host = "192.168.1.19", user = "pythonProgram", passwd = "woodensword", db = "example")
+cursor = connection.cursor()
+
+# create table
+cursor.execute ("drop table if exists employee") # produces error if there is no table, but not a screeching halt
+sql_command = """
+create table employee ( 
+    staff_number integer primary key, 
+    fname varchar(20), 
+    lname varchar(30), 
+    gender char(1), 
+    joining date,
+    birth_date date);
+"""
+cursor.execute(sql_command)
+
+# populate table
+staff_data = [
+    ("William", "Shakespeare", "m", "1961-10-25"),
+    ("Frank", "Schiller", "m", "1955-08-17"),
+    ("Jane", "Wall", "f", "1989-03-14")
+    ]
+
+format_str = """
+insert into employee (
+    staff_number, 
+    fname, 
+    lname, 
+    gender, 
+    birth_date) 
+values (
+    {staff_no}, 
+    '{first}', 
+    '{last}', 
+    '{gender}', 
+    '{birthdate}'
+);"""
+for staff, p in enumerate(staff_data):
+    sql_command = format_str.format(
+        staff_no=staff, 
+        first=p[0], 
+        last=p[1], 
+        gender=p[2], 
+        birthdate = p[3]
     )
-c = connection.cursor()
+    cursor.execute(sql_command)
 
-c.execute("SELECT * FROM numbers")
+# commit changes
+connection.commit()
 
-for item in c.fetchall():
-    print(item)
+# disconnect
+cursor.close()
+connection.close()
